2c0473d1d7c124e5e97b07d40714471d76e0fd6b
Furkan Topaloğlu <furkantopalogluu@gmail.com>
2022-10-20 16:56:11 +0300

    add mkdir command

---
+++ commands/mkdir/mkdir_test.go
@@ -0,0 +1,24 @@
+package mkdir_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/urfave/cli/v2"
+	"github.com/yakuter/gol/commands/mkdir"
+)
+
+func TestHelp(t *testing.T) {
+	execName, err := os.Executable()
+	require.NoError(t, err)
+
+	app := &cli.App{
+		Commands: []*cli.Command{
+			mkdir.Command(),
+		},
+	}
+
+	testArgs := []string{execName, "mkdir"}
+	require.NoError(t, app.Run(testArgs))
+}
